fb08760de0cd59a2dd4d0694e08afe61933284ef
pedropedroaugusto968-arch <pedropedroaugusto968@gmail.com>
2026-04-26 11:28:03 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,119 +1,146 @@
+#define UNICODE
 #include <windows.h>
 #include <tlhelp32.h>
-#include <iostream>
+#include <gdiplus.h>
+#include <vector>
+#include <string>
 
-// --- ESTADOS DAS FUNÇÕES ---
-bool bBypass = false;
-bool bAimbot = false;
-bool bFly = false;
-bool bEsp = false;
-bool bMinimized = false;
-int aimbotFov = 150;
+#pragma comment(lib, "gdiplus.lib")
+#pragma comment(lib, "gdi32.lib")
 
-// --- CORES ---
-#define COLOR_PURPLE RGB(160, 32, 240)
-#define COLOR_GRAY   RGB(60, 60, 60)
-#define COLOR_BLACK  RGB(15, 15, 15)
-#define COLOR_WHITE  RGB(255, 255, 255)
+using namespace Gdiplus;
 
-// --- FUNÇÃO BYPASS (ATTACH) ---
+// --- ESTRUTURA DE DADOS (SEU IMGUI + LÓGICA) ---
+struct HackState {
+    bool bBypass = false, bAimbot = false, bFly = false, bEsp = false;
+    int  aimbotFov = 150;
+    bool minimized = false;
+    int  activeTab = 0;
+    HANDLE hProcess = NULL;
+    uintptr_t baseAddress = 0;
+} S;
+
+// --- SCANNER DE PATTERN (ESTILO SEARCH DO GAME GUARDIAN) ---
+uintptr_t FindPattern(const BYTE* pattern, const char* mask) {
+    if (!S.hProcess) return 0;
+    
+    SYSTEM_INFO si;
+    GetSystemInfo(&si);
+    uintptr_t start = (uintptr_t)si.lpMinimumApplicationAddress;
+    uintptr_t end = (uintptr_t)si.lpMaximumApplicationAddress;
+    size_t patternLen = strlen(mask);
+
+    while (start < end) {
+        MEMORY_BASIC_INFORMATION mbi;
+        if (!VirtualQueryEx(S.hProcess, (LPCVOID)start, &mbi, sizeof(mbi))) break;
+
+        if (mbi.State == MEM_COMMIT && (mbi.Protect & PAGE_READWRITE || mbi.Protect & PAGE_EXECUTE_READWRITE)) {
+            std::vector<BYTE> buffer(mbi.RegionSize);
+            if (ReadProcessMemory(S.hProcess, mbi.BaseAddress, buffer.data(), mbi.RegionSize, NULL)) {
+                for (size_t i = 0; i < mbi.RegionSize - patternLen; i++) {
+                    bool found = true;
+                    for (size_t j = 0; j < patternLen; j++) {
+                        if (mask[j] == 'x' && buffer[i + j] != pattern[j]) {
+                            found = false; break;
+                        }
+                    }
+                    if (found) return (uintptr_t)mbi.BaseAddress + i;
+                }
+            }
+        }
+        start += mbi.RegionSize;
+    }
+    return 0;
+}
+
+// --- THREAD DE INJEÇÃO REAL-TIME ---
+DWORD WINAPI InjectionLoop(LPVOID lpParam) {
+    while (true) {
+        if (S.bBypass && S.hProcess) {
+            // LÓGICA DE FLY (Exemplo de Assinatura)
+            if (S.bFly) {
+                // Aqui você coloca os bytes que o GG acharia
+                const BYTE flySig[] = { 0xDE, 0xAD, 0xBE, 0xEF }; // SUBSTITUIR PELOS BYTES REAIS
+                uintptr_t addr = FindPattern(flySig, "xxxx");
+                if (addr) {
+                    float val = 500.0f;
+                    WriteProcessMemory(S.hProcess, (LPVOID)addr, &val, sizeof(val), NULL);
+                }
+            }
+            // Adicione aqui as outras funções (Aimbot, ESP, etc.)
+        }
+        Sleep(500); // Não sobrecarrega a CPU
+    }
+    return 0;
+}
+
+// --- FUNÇÃO DE ATTACH (SEU CÓDIGO) ---
 void ApplyBypass() {
-    HWND hwnd = FindWindowA(NULL, "Flex City"); // Nome da janela do jogo
+    HWND hwnd = FindWindowA(NULL, "Flex City");
     if (hwnd) {
-        DWORD procId;
-        GetWindowThreadProcessId(hwnd, &procId);
-        HANDLE hProc = OpenProcess(PROCESS_ALL_ACCESS, FALSE, procId);
-        if (hProc) {
-            bBypass = true;
-            CloseHandle(hProc);
+        DWORD pId;
+        GetWindowThreadProcessId(hwnd, &pId);
+        S.hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pId);
+        if (S.hProcess) {
+            S.bBypass = true;
+            CreateThread(NULL, 0, InjectionLoop, NULL, 0, NULL);
         }
     }
 }
 
-// --- DESENHO DA INTERFACE ---
-void DrawSwitch(HDC hdc, int x, int y, bool state) {
-    HBRUSH hBrush = CreateSolidBrush(state ? COLOR_PURPLE : COLOR_GRAY);
-    RECT r = { x, y, x + 40, y + 20 };
-    FillRect(hdc, &r, hBrush);
-    DeleteObject(hBrush);
-    
-    // Pequeno indicador dentro do switch
-    HBRUSH hKnob = CreateSolidBrush(COLOR_WHITE);
-    RECT k = { state ? x + 22 : x + 2, y + 2, state ? x + 38 : x + 18, y + 18 };
-    FillRect(hdc, &k, hKnob);
-    DeleteObject(hKnob);
+// [INÍCIO DO DESIGN SIRIUS GDI+]
+void DrawSwitch(Graphics& g, float x, float y, bool on, const wchar_t* label, RECT& hit) {
+    SolidBrush track(on ? Color(255, 138, 43, 226) : Color(255, 60, 60, 60));
+    g.FillRectangle(&track, x, y, 40, 20); // Simplificado para performance
+    SolidBrush knob(Color(255, 255, 255, 255));
+    g.FillRectangle(&knob, on ? x + 22 : x + 2, y + 2, 16, 16);
+    Font f(L"Segoe UI", 10.0f); SolidBrush b(Color(255, 255, 255, 255));
+    g.DrawString(label, -1, &f, PointF(x + 50, y), &b);
+    hit = { (LONG)x, (LONG)y, (LONG)x + 150, (LONG)y + 20 };
 }
 
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
-    switch (uMsg) {
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp) {
+    static RECT rcSw[4], rcTab[2], rcBall;
+    switch (msg) {
         case WM_PAINT: {
-            PAINTSTRUCT ps;
-            HDC hdc = BeginPaint(hwnd, &ps);
-            RECT rect; GetClientRect(hwnd, &rect);
-            HBRUSH hbg = CreateSolidBrush(COLOR_BLACK);
-            FillRect(hdc, &rect, hbg); DeleteObject(hbg);
-            HBRUSH hbr = CreateSolidBrush(COLOR_PURPLE);
-            FrameRect(hdc, &rect, hbr); DeleteObject(hbr);
-
-            SetBkMode(hdc, TRANSPARENT);
-            if (bMinimized) {
-                SetTextColor(hdc, COLOR_PURPLE);
-                TextOutA(hdc, 10, 15, "[ SPACE ]", 9);
+            PAINTSTRUCT ps; HDC hdc = BeginPaint(hwnd, &ps);
+            Graphics g(hdc);
+            if (S.minimized) {
+                SolidBrush p(Color(255, 138, 43, 226)); g.FillEllipse(&p, 0, 0, 50, 50);
+                rcBall = {0,0,50,50};
             } else {
-                SetTextColor(hdc, COLOR_PURPLE);
-                TextOutA(hdc, 45, 10, "SPACE XIT - PREMIUM", 19);
-                
-                SetTextColor(hdc, COLOR_WHITE);
-                TextOutA(hdc, 20, 50, "BYPASS GAME", 11); DrawSwitch(hdc, 180, 50, bBypass);
-                TextOutA(hdc, 20, 85, "AIMBOT", 6);       DrawSwitch(hdc, 180, 85, bAimbot);
-                TextOutA(hdc, 20, 120, "FLY MODE", 8);    DrawSwitch(hdc, 180, 120, bFly);
-                TextOutA(hdc, 20, 155, "ESP VISUAL", 10);  DrawSwitch(hdc, 180, 155, bEsp);
-                
-                char fov[20]; sprintf(fov, "FOV: %d", aimbotFov);
-                TextOutA(hdc, 20, 190, fov, strlen(fov));
+                SolidBrush bg(Color(255, 16, 14, 28)); g.FillRectangle(&bg, 0, 0, 300, 400);
+                DrawSwitch(g, 20, 60, S.bBypass, L"BYPASS", rcSw[0]);
+                DrawSwitch(g, 20, 100, S.bAimbot, L"AIMBOT", rcSw[1]);
+                DrawSwitch(g, 20, 140, S.bFly, L"FLY MODE", rcSw[2]);
+                DrawSwitch(g, 20, 180, S.bEsp, L"ESP", rcSw[3]);
             }
             EndPaint(hwnd, &ps); return 0;
         }
         case WM_LBUTTONDOWN: {
-            int x = LOWORD(lParam); int y = HIWORD(lParam);
-            if (!bMinimized) {
-                if (y > 45 && y < 75) ApplyBypass();
-                if (y > 80 && y < 110) bAimbot = !bAimbot;
-                if (y > 115 && y < 145) bFly = !bFly;
-                if (y > 150 && y < 180) bEsp = !bEsp;
-                InvalidateRect(hwnd, NULL, TRUE);
+            int mx = GET_X_LPARAM(lp), my = GET_Y_LPARAM(lp); POINT pt = {mx, my};
+            if (S.minimized) { S.minimized = false; SetWindowPos(hwnd, NULL, 0, 0, 300, 400, SWP_NOMOVE); }
+            else {
+                if (PtInRect(&rcSw[0], pt)) ApplyBypass();
+                if (PtInRect(&rcSw[1], pt)) S.bAimbot = !S.bAimbot;
+                if (PtInRect(&rcSw[2], pt)) S.bFly = !S.bFly;
+                if (PtInRect(&rcSw[3], pt)) S.bEsp = !S.bEsp;
             }
-            return 0;
+            InvalidateRect(hwnd, NULL, FALSE); return 0;
         }
         case WM_NCHITTEST: return HTCAPTION;
         case WM_DESTROY: PostQuitMessage(0); return 0;
     }
-    return DefWindowProc(hwnd, uMsg, wParam, lParam);
+    return DefWindowProc(hwnd, msg, wp, lp);
 }
 
-int WINAPI WinMain(HINSTANCE hI, HINSTANCE hP, LPSTR lp, int nS) {
-    WNDCLASS wc = { }; wc.lpfnWndProc = WindowProc; wc.hInstance = hI; wc.lpszClassName = "SpaceXit";
-    RegisterClass(&wc);
-    HWND hwnd = CreateWindowEx(WS_EX_TOPMOST | WS_EX_LAYERED, "SpaceXit", "SpaceXit", WS_POPUP, 100, 100, 250, 230, NULL, NULL, hI, NULL);
-    SetLayeredWindowAttributes(hwnd, 0, 255, LWA_ALPHA);
-    ShowWindow(hwnd, nS);
-
-    MSG msg = { };
-    while (true) {
-        if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
-            if (msg.message == WM_QUIT) break;
-            TranslateMessage(&msg); DispatchMessage(&msg);
-        }
-        if (GetAsyncKeyState(VK_F2) & 1) {
-            bMinimized = !bMinimized;
-            SetWindowPos(hwnd, NULL, 0, 0, bMinimized ? 80 : 250, bMinimized ? 50 : 230, SWP_NOMOVE | SWP_NOZORDER);
-            InvalidateRect(hwnd, NULL, TRUE);
-        }
-        if (!bMinimized) {
-            if (GetAsyncKeyState(VK_RIGHT) & 0x8000) { if (aimbotFov < 500) aimbotFov += 2; InvalidateRect(hwnd, NULL, TRUE); }
-            if (GetAsyncKeyState(VK_LEFT) & 0x8000) { if (aimbotFov > 0) aimbotFov -= 2; InvalidateRect(hwnd, NULL, TRUE); }
-        }
-        Sleep(10);
-    }
-    return 0;
+int WINAPI WinMain(HINSTANCE hI, HINSTANCE, LPSTR, int nS) {
+    GdiplusStartupInput gsi; ULONG_PTR gst; GdiplusStartup(&gst, &gsi, NULL);
+    WNDCLASSW wc = {0}; wc.lpfnWndProc = WndProc; wc.hInstance = hI; wc.lpszClassName = L"SpaceXit";
+    RegisterClassW(&wc);
+    HWND hwnd = CreateWindowExW(WS_EX_TOPMOST | WS_EX_LAYERED, L"SpaceXit", L"SpaceXit", WS_POPUP, 100, 100, 300, 400, NULL, NULL, hI, NULL);
+    SetLayeredWindowAttributes(hwnd, 0, 255, LWA_ALPHA); ShowWindow(hwnd, nS);
+    MSG msg;
+    while (GetMessage(&msg, NULL, 0, 0)) { TranslateMessage(&msg); DispatchMessage(&msg); }
+    GdiplusShutdown(gst); return 0;
 }
